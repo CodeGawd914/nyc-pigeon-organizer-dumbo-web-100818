f44d8d4976e37b80e4f4793b11dd370b4b063838
Travis Desir <travisgdesir31@gmail.com>
2018-09-25 20:05:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -33,10 +33,10 @@
 # end
 
 def nyc_pigeon_organizer(data)
- new_pigeon_data = {}
-   data.each do |attribute_category, attribute_data_hash|
-    attribute_data_hash.each do |attribute, name_array|
-      name_array.each do |name|
+ haxh = {}
+   data.each do |key, value|
+    value.each do |x,y|
+      y.each do |name|
         new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}  #establish top tier
       end
     end
