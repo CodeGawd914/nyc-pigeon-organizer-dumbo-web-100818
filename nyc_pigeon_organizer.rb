2496c1f9618a7fe586bd8cc41fe81c41209736fb
Travis Desir <travisgdesir31@gmail.com>
2018-09-25 19:10:37 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,10 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   haxh = {}
   data.each do |key,value|
     value. each do |x,y|
       y.each do |name|
+        binding.pry
         haxh[name]= {:color => [], :gender => [], :lives => []}
       end
     end
