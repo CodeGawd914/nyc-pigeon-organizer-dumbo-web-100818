9519dbe676a049e3c148e8c574d36b752efd9b13
Travis Desir <travisgdesir31@gmail.com>
2018-09-25 20:01:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,33 +1,33 @@
-require 'pry'
-def nyc_pigeon_organizer(data)
-  haxh = {}
-  data.each do |key,value|
-    value. each do |x,y|
-      y.each do |name|
-        haxh[name]= {:color => [], :gender => [], :lives => []}
-      end
-    end
-  end
-    data[:color].each do |key,value|
-      value.each do |name|
-        if data[:color][key].include?(name)
-          haxh[name][:color] << key.to._s
-        end
-      end
-    end
-    data[:gender].each do |key,value|
-      value.each do |gender|
-        if data[:gender][key].include?(gender)
-          haxh[gender][:gender] << key.to_s
-        end
-      end
-    end
-    data[:lives].each do |key,value|
-      value.each do |place|
-        if data[:lives][key].include?(place)
-          haxh[place][:lives] << key.to_s
-        end
-      end
-    end
-    haxh
-end
+# require 'pry'
+# def nyc_pigeon_organizer(data)
+#   haxh = {}
+#   data.each do |key,value|
+#     value. each do |x,y|
+#       y.each do |name|
+#         haxh[name]= {:color => [], :gender => [], :lives => []}
+#       end
+#     end
+#   end
+#     data[:color].each do |key,value|
+#       value.each do |name|
+#         if data[:color][key].include?(name)
+#           haxh[name][:color] << key.to._s
+#         end
+#       end
+#     end
+#     data[:gender].each do |key,value|
+#       value.each do |gender|
+#         if data[:gender][key].include?(gender)
+#           haxh[gender][:gender] << key.to_s
+#         end
+#       end
+#     end
+#     data[:lives].each do |key,value|
+#       value.each do |place|
+#         if data[:lives][key].include?(place)
+#           haxh[place][:lives] << key.to_s
+#         end
+#       end
+#     end
+#     haxh
+# end
